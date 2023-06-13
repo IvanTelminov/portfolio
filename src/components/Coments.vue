d85d0c7edb66b7073cc2170ae92d03819980d962
ivan <ivan@telminov.ru>
2023-06-13 10:18:15 +0300

    Заготовка проекта

---
+++ src/components/Coments.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  coments: Array,
+})
+</script>
+
+<template>
+<ul>
+  <li v-for="c in coments">
+    {{c}}
+  </li>
+</ul>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Photos.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  header: String,
+  urls: String,
+  text: String
+})
+import {ref} from "vue";
+const inputRef = ref(null)
+const emit = defineEmits(['comCreated'])
+function createCom() {
+  const inputElement = inputRef.value
+  const coments = inputElement.value
+  inputElement.value = ''
+  emit("comCreated", coments)
+}
+</script>
+
+
+
+<template>
+ <h1>{{header}}</h1>
+  <img :src="urls" />
+  <p>{{text}}</p>
+  <input
+      ref="inputRef"
+      placeholder="коментарии"
+      @keyup.enter="createCom"/>
+
+  <hr>
+
+</template>
+
+<style scoped>
+img {
+  height: 200px;
+}
+</style>
